286a8969408096e534cd03c05cbcdb3b8f3403f6
anvarovagulnaz <anvarovagulnaz@gmail.com>
2022-03-20 18:09:38 +0500

    Добавьте файлы проекта.

---
+++ RevitAPI_Lesson2_4/DuctFilter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPI_Lesson2_4
+{
+    class DuctFilter
+    {
+    }
+}
